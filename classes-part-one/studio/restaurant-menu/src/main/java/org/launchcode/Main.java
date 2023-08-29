38f9201d4266d4cf07cb4ae078fe85d31060dcdf
NanitaAgarwal <nanita.agarwal@gmail.com>
2023-08-28 19:55:00 -0500

    Classes-Part-One Studio - Design only

--- classes-part-one/studio/restaurant-menu/src/main/java/org/launchcode/Main.java
+++ classes-part-one/studio/restaurant-menu/src/main/java/org/launchcode/Main.java
@@ -6,3 +6,43 @@ public class Main {
 	// write your code here
     }
 }
+
+//  Below is the design we have created as part of the Studio on 08/28/2023.
+//  ------------------------------------------------------------------------
+/*
+
+    Class Menu:
+        Properties:
+            Private Date lastUpdated
+            Private ArrayList<MenuItem> mainMenu
+        Method:
+            Getters:
+                Public Date getLastUpdated()
+            Setters:
+                Private Date setLastUpdated()
+            Other Methods:
+                addNewMenuItem()
+                removeMenuItem()
+                displayMenu()
+// This contains sub-methods that (1) organize the menu by category and (2) checks if the date added is recent compared to today
+
+    Class MenuItem:
+        Properties:
+            Private Double Price
+            Private String Description
+            Private String Category
+            Private Date dateAdded
+        Methods:
+            Getters:
+                Public Double getPrice()
+                Public String getDescription()
+                Public String getCategory()
+            Setters:
+                setPrice()
+                setDescription()
+                setCategory()
+                setDateAdded()
+
+*/
+
+

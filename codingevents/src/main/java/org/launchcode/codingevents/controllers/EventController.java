e20347b5f37d5517ad92abf1a83d75fb19e3bd00
NanitaAgarwal <nanita.agarwal@gmail.com>
2023-10-30 12:55:29 -0500

    codingevents-video2-Add Form Handler Method

--- codingevents/src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ codingevents/src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -3,7 +3,9 @@ package org.launchcode.codingevents.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,16 +15,18 @@ import java.util.List;
 //@RequestMapping(value="events",method = {RequestMethod.GET})
 public class EventController {
 
+    private static List<String> events = new ArrayList<>();
+
     @GetMapping
     public String displayAllEvents(Model model) {
-        List<String> events = new ArrayList<>();
-        events.add("Independence Day Event");
-        events.add("Ganesh Festival");
-        events.add("Navrathri Dandiya");
-        events.add("Easter Egg Hunt");
-        events.add("Thanks Giving Dinner");
-        events.add("Christmas Secret Santa");
-        events.add("New Years Eve Party");
+//        List<String> events = new ArrayList<>();
+//        events.add("Independence Day Event");
+//        events.add("Ganesh Festival");
+//        events.add("Navrathri Dandiya");
+//        events.add("Easter Egg Hunt");
+//        events.add("Thanks Giving Dinner");
+//        events.add("Christmas Secret Santa");
+//        events.add("New Years Eve Party");
         model.addAttribute("events",events);
         return "events/index";
     }
@@ -32,4 +36,9 @@ public class EventController {
         return "events/create";
     }
 
+    @PostMapping("create")
+    public String createEvent(@RequestParam String eventName){
+        events.add(eventName);
+        return "redirect:/events";
+    }
 }
